716d8410f01f426b89d4cc3f78614bae1f343ab3
mung3r <rodney@bighatchet.com>
2011-11-19 10:31:07 -0800

    removed unused imports

--- src/main/java/com/edwardhand/mobrider/commands/BaseCommand.java
+++ src/main/java/com/edwardhand/mobrider/commands/BaseCommand.java
@@ -7,73 +7,73 @@ package com.edwardhand.mobrider.commands;
 import java.util.ArrayList;
 import java.util.List;
 
-import net.minecraft.server.Entity;
-import net.minecraft.server.EntityCreature;
-
 import org.bukkit.command.CommandSender;
-import org.bukkit.craftbukkit.entity.CraftPlayer;
-import org.bukkit.entity.Player;
 
 public abstract class BaseCommand
 {
     protected String permission;
-	protected String usage;
-	protected int minArgs;
-	protected int maxArgs;
-	protected List<String> identifiers = new ArrayList<String>();;
-	
-	public abstract void execute(CommandSender commandSender, String[] args);
+    protected String usage;
+    protected int minArgs;
+    protected int maxArgs;
+    protected List<String> identifiers = new ArrayList<String>();;
 
-	public String[] validate(String input, StringBuilder identifier) {
-		String match = matchIdentifier(input);
+    public abstract void execute(CommandSender commandSender, String[] args);
 
-		if (match != null) {
-			identifier = identifier.append(match);
-			int i = identifier.length();
-			String[] args = input.substring(i).trim().split(" ");
-			if (args[0].isEmpty()) {
-				args = new String[0];
-			}
-			int l = args.length;
-			if ((l >= this.minArgs) && (l <= this.maxArgs)) {
-				return args;
-			}
-		}
-		return null;
-	}
+    public String[] validate(String input, StringBuilder identifier)
+    {
+        String match = matchIdentifier(input);
 
-	public String matchIdentifier(String input) {
-		String lower = input.toLowerCase();
+        if (match != null) {
+            identifier = identifier.append(match);
+            int i = identifier.length();
+            String[] args = input.substring(i).trim().split(" ");
+            if (args[0].isEmpty()) {
+                args = new String[0];
+            }
+            int l = args.length;
+            if ((l >= this.minArgs) && (l <= this.maxArgs)) {
+                return args;
+            }
+        }
+        return null;
+    }
 
-		int index = -1;
-		int n = this.identifiers.size();
-		for (int i = 0; i < n; i++) {
-			String identifier = ((String)this.identifiers.get(i)).toLowerCase();
-			if (lower.matches(identifier + "(\\s+.*|\\s*)")) {
-				index = i;
-			}
-		}
+    public String matchIdentifier(String input)
+    {
+        String lower = input.toLowerCase();
 
-		if (index != -1) {
-			return (String)this.identifiers.get(index);
-		}
-		return null;
-	}
+        int index = -1;
+        int n = this.identifiers.size();
+        for (int i = 0; i < n; i++) {
+            String identifier = ((String) this.identifiers.get(i)).toLowerCase();
+            if (lower.matches(identifier + "(\\s+.*|\\s*)")) {
+                index = i;
+            }
+        }
 
-	public List<String> getIdentifiers()
-	{
-		return this.identifiers;
-	}
+        if (index != -1) {
+            return (String) this.identifiers.get(index);
+        }
+        return null;
+    }
 
-	public void setIdentifiers(List<String> identifiers) {
-		this.identifiers = identifiers;
-	}
+    public List<String> getIdentifiers()
+    {
+        return this.identifiers;
+    }
 
-	public String getUsage() {
-		return this.usage;
-	}
+    public void setIdentifiers(List<String> identifiers)
+    {
+        this.identifiers = identifiers;
+    }
 
-	public String getPermission() {
-	    return this.permission;
-	}
+    public String getUsage()
+    {
+        return this.usage;
+    }
+
+    public String getPermission()
+    {
+        return this.permission;
+    }
 }
