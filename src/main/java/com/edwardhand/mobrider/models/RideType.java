675511dc300ae368a61f166c481cfbbba64a2a7e
mung3r <rodney@bighatchet.com>
2011-12-16 23:22:57 -0800

    adjusted mob speeds

--- src/main/java/com/edwardhand/mobrider/models/RideType.java
+++ src/main/java/com/edwardhand/mobrider/models/RideType.java
@@ -6,27 +6,27 @@ import java.util.Map;
 import org.bukkit.entity.CreatureType;
 
 public enum RideType {
-    BLAZE(CreatureType.BLAZE, 0.0F, "Kaboom"),
+    BLAZE(CreatureType.BLAZE, 0.4F, "Kaboom"),
     CAVE_SPIDER(CreatureType.CAVE_SPIDER, 0.4F, "Cheeuuuk"),
     CHICKEN(CreatureType.CHICKEN, 0.2F, "Cluck"),
     COW(CreatureType.COW, 0.3F, "Mooooo"),
     CREEPER(CreatureType.CREEPER, 0.6F, "Hisssss"),
-    ENDER_DRAGON(CreatureType.ENDER_DRAGON, 0.0F, "Rawwwwwwr"),
+    ENDER_DRAGON(CreatureType.ENDER_DRAGON, 0.9F, "Rawwwwwwr"),
     ENDERMAN(CreatureType.ENDERMAN, 0.2F, "Mooooaaan"),
     GHAST(CreatureType.GHAST, 0.6F, "Hooooo"),
     GIANT(CreatureType.GIANT, 0.6F, "MOOOOAAAN"),
     MONSTER(CreatureType.MONSTER, 0.4F, "Rawr"),
-    MUSHROOM_COW(CreatureType.MUSHROOM_COW, 0.0F, "Moo00sh"),
+    MUSHROOM_COW(CreatureType.MUSHROOM_COW, 0.3F, "Moo00sh"),
     PIG(CreatureType.PIG, 0.25F, "Oink"),
     PIG_ZOMBIE(CreatureType.PIG_ZOMBIE, 0.4F, "Mooooiiink"),
     SHEEP(CreatureType.SHEEP, 0.25F, "Baaaaa"),
-    SILVERFISH(CreatureType.SILVERFISH, 0.4F, "Cheeuuuk"),
+    SILVERFISH(CreatureType.SILVERFISH, 0.2F, "Cheeuuuk"),
     SKELETON(CreatureType.SKELETON, 0.2F, "*silence*"),
     SLIME(CreatureType.SLIME, 0.2F, "Blluuurrrp"),
     SPIDER(CreatureType.SPIDER, 0.4F, "Cheeuuuk"),
-    SQUID(CreatureType.SQUID, 1.0F, "Glub"),
-    VILLAGER(CreatureType.VILLAGER, 0.0F, "Mhmm"),
-    WOLF(CreatureType.WOLF, 0.8F, "Woof"),
+    SQUID(CreatureType.SQUID, 0.7F, "Glub"),
+    VILLAGER(CreatureType.VILLAGER, 0.3F, "Mhmm"),
+    WOLF(CreatureType.WOLF, 0.7F, "Woof"),
     ZOMBIE(CreatureType.ZOMBIE,0.2F, "Mooooaaan");
 
     private CreatureType type;
