d17ae5401f75a05aa54d8359650a885e1459882b
mung3r <rodney@bighatchet.com>
2012-10-28 12:29:06 -0700

    added LGPL license

--- src/main/java/com/edwardhand/mobrider/commands/MountCommand.java
+++ src/main/java/com/edwardhand/mobrider/commands/MountCommand.java
@@ -1,3 +1,22 @@
+/*
+ * This file is part of MobRider.
+ *
+ * Copyright (c) 2011-2012, R. Ramos <http://github.com/mung3r/>
+ * MobRider is licensed under the GNU Lesser General Public License.
+ *
+ * MobRider is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MobRider is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.edwardhand.mobrider.commands;
 
 import org.bukkit.Material;
