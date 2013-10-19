8ebe3be8c2dd995268e4291b0bddabee41a7b42b
mung3r <mung3r@bighatchet.com>
2013-10-18 17:43:42 -0700

    restored bukkit and vault version updates

--- src/main/java/com/edwardhand/mobrider/goals/search/strategies/EntityIdSearchStrategy.java
+++ src/main/java/com/edwardhand/mobrider/goals/search/strategies/EntityIdSearchStrategy.java
@@ -19,9 +19,9 @@
  */
 package com.edwardhand.mobrider.goals.search.strategies;
 
-import net.minecraft.server.v1_6_R2.Entity;
+import net.minecraft.server.v1_6_R3.Entity;
 
-import org.bukkit.craftbukkit.v1_6_R2.CraftWorld;
+import org.bukkit.craftbukkit.v1_6_R3.CraftWorld;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 
